b2e733934246121c272ee387c9bb541e42cee014
aleks1303 <aleksbulda13@gmail.com>
2025-05-21 12:25:42 +0300

    init

--- src/components/pagination-component/PaginationComponent.tsx
+++ src/components/pagination-component/PaginationComponent.tsx
@@ -1,10 +1,12 @@
+import {useSearchParams} from "react-router-dom";
 
 
 const PaginationComponent = () => {
-
+const [query, setQuery] = useSearchParams({page: '1'});
     return (
         <div>
-
+            <button></button>
+            <button></button>
         </div>
     );
 };
