9f8dd79830fde1b44692ef41370af1df40adea9f
aleks1303 <aleksbulda13@gmail.com>
2025-05-28 11:51:45 +0300

    init

--- src/components/movie-delails-components/MovieDetailsComponent.tsx
+++ src/components/movie-delails-components/MovieDetailsComponent.tsx
@@ -1,49 +1,11 @@
 import {useParams} from "react-router-dom";
 import StarsRating from "../renting-components/ReatingComponent.tsx";
-
-
-// const MovieDetailsComponent = () => {
-//    // const { genre } = useAppSelector(({genreSlice})=> genreSlice);
-//   const [id] = useParams();
-//
-//     if (!movie) {
-//         return ("Фільм не передано")
-//     }
-//     const genreNames = movie.genres?.map(g => g.name) || []
-//     return (
-//         <div className={'flex bg-orange-50 pb-5'}>
-//             <div className={'m-2'}>
-//                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
-//             </div>
-//             <div className={'text-5xl ml-10 pt-2'}>
-//                 <h1 className={'underline mb-4'}>{movie.title}</h1>
-//                 <div className={'text-sm pl-2 underline text-blue-800'}>
-//                     {genreNames.map((name, index) => (
-//                     <span className={'mr-2'} key={index}>{name}</span>))}
-//                 </div>
-//
-//                 <div className={'text-sm pl-2 underline text-blue-800'}>
-//                     {genreNames.map((name, index) => (
-//                         <span className={'mr-2'} key={index}>{name}</span>
-//                     ))}
-//                 </div>
-//
-//
-//                 <div className="mt-2 pl-2"><StarsRating item={movie.vote_average}/></div>
-//             </div>
-//         </div>
-//
-//     );
-// };
-//
-
-
 import {useEffect} from "react";
-
-
 import {useAppDispatch} from "../../redux/hooks/useAppDispatch.ts";
 import {useAppSelector} from "../../redux/hooks/useAppSelector.ts";
 import {movieSliceActions} from "../../redux/slices/movie-slice/movieSlice.tsx";
+
+
 
 const MovieDetailsComponent = () => {
     const {id} = useParams();
