c52ff558161f73105d75157de7c12be1c31a5abf
aleks1303 <aleksbulda13@gmail.com>
2025-05-28 19:23:22 +0300

    init

--- src/components/movie-delails-components/MovieDetailsComponent.tsx
+++ src/components/movie-delails-components/MovieDetailsComponent.tsx
@@ -23,14 +23,14 @@ const MovieDetailsComponent = () => {
     if (!movie) return <div>Фільм не знайдено</div>;
 
     return (
-        <div className={"p-3 bg-orange-50 flex gap-6"}>
+        <div className={"p-3 bg-gray-100 flex gap-6"}>
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-125"/>
             <div>
                 <h1 className={"text-4xl underline"}>{movie.title}</h1>
                 <p>{movie.tagline}</p>
                 <div className={"mt-2 flex gap-2 flex-wrap"}>
                     {movie.genres.map(g => (
-                        <span key={g.id} className={"text-sm bg-orange-200 text-orange-800 px-2 py-1 rounded-full"}>
+                        <span key={g.id} className={"text-sm bg-gray-600 text-white px-2 py-1 rounded-full"}>
                             {g.name}
                         </span>
                     ))}
@@ -42,17 +42,17 @@ const MovieDetailsComponent = () => {
                         <span className={'text-sm ml-1'}>{movie.vote_count}</span>
                     </p>
                 </div>
-                <div className={"mt-4 bg-orange-100"}>Description:<p>{movie.overview}</p></div>
+                <div className={"mt-4 bg-gray-300"}>Description:<p>{movie.overview}</p></div>
                 <p className={'mt-4'}>Budget: {movie.budget} $</p>
                 <p >Revenue: {movie.revenue} $</p>
-                <p className={'mt-4 bg-orange-100'}>Country: {movie.production_countries.map((country, index) =>
+                <p className={'mt-4 bg-gray-300'}>Country: {movie.production_countries.map((country, index) =>
                     (<CountryProductComponent key={index} country={country}/>))}</p>
                 <p className={'mt-4'}>Product: {movie.production_companies.map(company =>
                     (<CompanyComponent key={company.id} company={company}/>))}</p>
-                <p className={'mt-4 bg-orange-100'}>Language: {movie.spoken_languages.map((language, index) =>
+                <p className={'mt-4 bg-gray-300'}>Language: {movie.spoken_languages.map((language, index) =>
                     (<LanguageComponent key={index} language={language}/>))}</p>
                 <p className={'mt-4'}>Release date: {movie.release_date}</p>
-                <p className={'mt-4 bg-orange-100'}>Runtime: {movie.runtime} min</p>
+                <p className={'mt-4 bg-gray-300'}>Runtime: {movie.runtime} min</p>
             </div>
         </div>
     );
